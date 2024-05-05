b1d5faa7bfd5d914b336ee15ce6cd3a569ab7489
raitank4 <konsta.raitanen@aalto.fi>
2024-05-05 10:49:42 +0300

    navbar mostly working, fonts working

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,10 +4,10 @@ import '../fonts.css';
 function Navbar () {
     return (
         <nav className="navbar">
-            <p><a className="navlink">Ylelogo</a></p>
+            <p><a className="navlink" href="koti">Ylelogo</a></p>
             <p>
-                <a className="navlink">Mistä on kyse</a>
-                <a className="button">Testaa medialukutaitosi!</a>
+                <a className="navlink" href="info">Mistä on kyse?</a>
+                <a className="button" href="testi">Testaa medialukutaitosi!</a>
             </p>
         </nav>
     )
